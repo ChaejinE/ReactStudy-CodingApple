4538345386aba7da3e10cb9443cfed8788d56abb
cjlotto <joung6517@gmail.com>
2023-04-24 22:02:17 +0900

    shop settings

---
+++ shop/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>{' '}
+    </div>
+  );
+}
+
+export default App;
