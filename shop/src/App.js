593db17a869f452112d3e4a2505753319a99d875
cjlotto <joung6517@gmail.com>
2023-04-24 22:51:45 +0900

    Card map, props 써서 만들기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import data from "./data.js"
+import { useState } from 'react';
 
 function App() {
+  let [shoes, setShoes] = useState(data);
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{' '}
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Shoes</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+      <div className="container">
+        <div className='row'>
+          {
+            shoes.map((shoe, idx) => {
+              return (
+                <MyComponent shoe={shoe} key={idx} />
+              );
+            })
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MyComponent(props) {
+  return (
+    <div className="col-md-4" key={props.key}>
+      <img src={props.shoe.url} width="80%" />
+      <h4>{props.shoe.title}</h4>
+      <p>{props.shoe.price}</p>
     </div>
   );
 }
---
+++ shop/src/data.js
@@ -0,0 +1,27 @@
+let data = [
+    {
+        id: 0,
+        url: "https://codingapple1.github.io/shop/shoes1.jpg",
+        title: "White and Black",
+        content: "Born in France",
+        price: 120000
+    },
+
+    {
+        id: 1,
+        url: "https://codingapple1.github.io/shop/shoes2.jpg",
+        title: "Red Knit",
+        content: "Born in Seoul",
+        price: 110000
+    },
+
+    {
+        id: 2,
+        url: "https://codingapple1.github.io/shop/shoes3.jpg",
+        title: "Grey Yordan",
+        content: "Born in the States",
+        price: 130000
+    }
+]
+
+export default data
