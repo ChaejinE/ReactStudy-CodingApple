eb6158e94c06e58e92900e1b5f132561e10a2812
cjlotto <joung6517@gmail.com>
2023-04-26 21:43:30 +0900

    styled-component

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -1,4 +1,11 @@
 import { useParams } from "react-router-dom";
+import styled from 'styled-components'
+
+let Btn = styled.button`
+    background: ${props => props.bg};
+    color : ${props => (props.bg == "blue") ? "white" : "black"};
+    padding : 1px;
+  `
 
 function Detail(props) {
     let { id } = useParams();
@@ -7,6 +14,7 @@ function Detail(props) {
 
     return (
         <div className="container">
+            <Btn bg="blue">버튼</Btn>
             <div className="row">
                 <div className="col-md-6">
                     <img src={"https://codingapple1.github.io/shop/shoes" + (parseInt(id) + 1) + ".jpg"} width="100%" />
